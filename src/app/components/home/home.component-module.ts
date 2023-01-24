235636e0683be0bcd8074f5bc794b5b3f4d96ede
Magda Ramotowska <rtkmagdalena@gmail.com>
2023-01-24 14:12:32 +0100

    feature: empty pages created and added to routes

---
+++ src/app/components/home/home.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  imports: [RouterModule],
+  declarations: [HomeComponent],
+  providers: [],
+  exports: [HomeComponent],
+})
+export class HomeComponentModule {}
