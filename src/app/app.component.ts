6e58044cee71efbce77de31c8dee40c2166408e7
Magda Ramotowska <rtkmagdalena@gmail.com>
2023-01-24 15:33:45 +0100

    feature: page layout - header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryModel } from './models/category.model';
+import { StoreModel } from './models/store.model';
+import { CategoriesService } from './services/categories.service';
+import { StoresService } from './services/stores.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-freshcard-bootstrap-theme';
+
+  readonly categories$: Observable<CategoryModel[]> =
+    this._categoriesService.getAllCategories();
+  readonly stores$: Observable<StoreModel[]> = this._storesService.getAllStores();
+
+  constructor(private _categoriesService: CategoriesService, private _storesService: StoresService) { }
 }
