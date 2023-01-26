0e916850cebab841d9962309c6edc6fca80f8648
Magda Ramotowska <rtkmagdalena@gmail.com>
2023-01-26 14:54:04 +0100

    show all stores in home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,29 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { CategoryModel } from '../../models/category.model';
+import { StoreModel } from '../../models/store.model';
 import { CategoriesService } from '../../services/categories.service';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly categories$: Observable<CategoryModel[]> =
+    this._categoriesService.getAllCategories();
+  readonly stores$: Observable<StoreModel[]> =
+    this._storesService.getAllStores();
 
-  constructor(private _categoriesService: CategoriesService) {
-  }
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _storesService: StoresService
+  ) {}
 }
