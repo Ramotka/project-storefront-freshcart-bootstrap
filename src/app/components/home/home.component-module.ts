3621b883b9a7c1cad97021f9b09983319c0e826c
Magda Ramotowska <rtkmagdalena@gmail.com>
2023-01-26 14:31:24 +0100

    show all categories in home page

--- src/app/components/home/home.component-module.ts
+++ src/app/components/home/home.component-module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   declarations: [HomeComponent],
   providers: [],
   exports: [HomeComponent],
 })
-export class HomeComponentModule {}
+export class HomeComponentModule { }
